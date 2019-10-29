48dc5f9fa574764b8e82fa6d6e1e953fce933b80
Anna Mazurek <anna.mazurek.93@gmain.com>
2019-10-29 09:30:40 +0100

    Main function

--- display.py
+++ display.py
@@ -1,1 +1,7 @@
 # tu funkcje, które coś printują
+
+
+def print_table(albums):
+    for album in albums:
+        print(album)
+
--- music_reports.py
+++ music_reports.py
@@ -1,13 +1,26 @@
 # CODECOOL MUSIC LIBRARY
 
-def jakasfunkcja():
-    pass
+import display
+# albums_dict = {artist name, album name, release year, genre, length}
 
+
+def read_albums(file_path):
+    data = []
+
+    with open(file_path) as file:
+        lines = file.readlines()
+        
+    for line in lines:
+        splitted_line = line.strip().split(',')
+        splitted_line[2] = int(splitted_line[2])
+        data.append(splitted_line)
+        
+    return data
 
 def main():
-    pass
+    path = 'text_albums_data.txt'
+    albums = read_albums(path)
+    display.print_table(albums)
 
 
 main()
-
-# 
