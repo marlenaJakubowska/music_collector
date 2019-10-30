77e1767b1bd0b725bc096cc76a1ce3ef125ba272
Anna Mazurek <anna.mazurek.93@gmain.com>
2019-10-30 10:37:00 +0100

    Fix program running

--- display.py
+++ display.py
@@ -2,8 +2,7 @@
 
 
 def print_table(albums):
-    for album in albums:
-        print(album)
+
 
 # print table
     print("-" * 101 + "\n" + " | ".join(("artist name".center(30), "album name".center(30), "year".center(4), "genre".center(18), "length".center(6))) + "\n" + "-" * 101)
@@ -11,7 +10,7 @@ def print_table(albums):
     for artist_name, album_name, release_year, genre, length in albums:
         print(' | '.join((artist_name.ljust(30), album_name.ljust(30), str(release_year).ljust(4), genre.ljust(18), length.ljust(6))))
     
-    print('-' * 100)
+    print('-' * 101)
 
 
 # album not found
