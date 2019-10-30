91b25932443eb17e66c52c99cb40f94551ccc805
Anna Mazurek <anna.mazurek.93@gmain.com>
2019-10-30 10:08:09 +0100

    Fix table print

--- display.py
+++ display.py
@@ -6,10 +6,10 @@ def print_table(albums):
         print(album)
 
 # print table
-    print("-" * 100 + "\n" + " | ".join(("artist name".center(30), "album name".center(30), "year".center(4), "genre".center(18), "length".center(6))) + "\n" + "-" * 100)
+    print("-" * 101 + "\n" + " | ".join(("artist name".center(30), "album name".center(30), "year".center(4), "genre".center(18), "length".center(6))) + "\n" + "-" * 101)
 
     for artist_name, album_name, release_year, genre, length in albums:
-        print(' | '.join((album_name.ljust(30), album_name.ljust(30), str(release_year).ljust(4), genre.ljust(18), length.ljust(6))))
+        print(' | '.join((artist_name.ljust(30), album_name.ljust(30), str(release_year).ljust(4), genre.ljust(18), length.ljust(6))))
     
     print('-' * 100)
 
